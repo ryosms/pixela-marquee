9ecfa1daff41707b312002f41e46755caee10040
Ryo Sumasu <ryo.sms@gmail.com>
2018-11-08 14:23:41 +0900

    Setup build.gradle for java application

---
+++ src/main/java/com/ryosms/pixela_marquee/PixelaMarqueeApp.java
@@ -0,0 +1,11 @@
+package com.ryosms.pixela_marquee;
+
+public class PixelaMarqueeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Pixela Marquee!");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
